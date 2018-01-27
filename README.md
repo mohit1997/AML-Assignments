0f387b355a610a8f5633d45fbf7ac00a73b1ce94
Mohit Goyal <goyal.mohit999@gmail.com>
2018-01-28 03:21:28 +0530

    Adding to readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AML-Assignments
+
+# For Batch Normalization follow https://wiki.tum.de/display/lfdv/Batch+Normalization#BatchNormalization-[2]BatchNormalization
---
+++ get_data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+s1="mnist"
+s2="emnist"
+
+if [ "$1" = "$s1" ]; then
+  echo downloading mnist
+  wget -A gz -R html,tmp  -r -l 1 -nd http://yann.lecun.com/exdb/mnist/
+fi
+
+if [ "$1" = "$s2" ]; then
+  echo downloading mnist
+  wget https://cloudstor.aarnet.edu.au/plus/index.php/s/54h3OuGJhFLwAlQ/download
+fi
